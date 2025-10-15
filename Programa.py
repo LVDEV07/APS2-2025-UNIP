329f3be98d1443f4310e7e8423118150da7a2214
LVDEV07 <lucas.fst.dev@gmail.com>
2025-10-15 19:53:18 -0300

    Criacao menu.py e modificacao program.py

---
+++ Programa.py
@@ -0,0 +1,28 @@
+import menu
+
+menu.menu()
+
+opcaoEscolhida = int(input("\nDigite o número da opção que você deseja acessar (1 a 6):"))
+
+if opcaoEscolhida < 1 or opcaoEscolhida >6:
+    menu.selecionarOpcao(opcaoEscolhida)
+
+
+
+
+
+
+#Menu de opções:
+
+#Criar Arquivo
+#Ler Arquivo
+#Criptografar Arquivo
+#Descriptografar Arquivo
+#Sair
+
+
+
+
+
+
+
---
+++ menu.py
@@ -0,0 +1,42 @@
+def menu():
+
+        print("Seja Bem-Vindo ao nosso programa ! \n")
+
+        print("Menu de opções: \n")
+
+        print("1- Criar Arquivo")
+        print("2- Escrever no Arquivo")
+        print("3- Ler Arquivo")
+        print("4- Criptografar Arquivo")
+        print("5 - Descriptografar Arquivo")
+        print("6- Sair")
+
+
+def criarArquivo(nomeArquivo):
+    print()
+
+def escreverArquivo(nomeArquivo, Texto):
+    print()
+
+def lerArquivo(nomeArquivo):
+    print()
+
+def criptografarArquivo(nomerquivo):
+    print()
+
+def descriptografarArquivo(nomerquivo):
+    print()
+
+def selecionarOpcao(opcaoEscolhida):
+    
+
+    while opcaoEscolhida < 1 or opcaoEscolhida >6:
+        print("\n-------------Opção Incorreta, tente novamente:-------------\n")
+                
+        opcaoEscolhida = int(input("\nDigite a opção que você deseja acessar (1 a 6): "))
+
+
+
+
+
+
